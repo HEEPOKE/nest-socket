b7f85ad4b3275514f5a8fd45b91e17b27a7d1120
Poundpitak Amornlerdlittichai <Damon1fx@gmail.com>
2024-06-05 14:48:02 +0700

    feat: check :money_with_wings:

--- src/apis/modules/events/events.gateway.ts
+++ src/apis/modules/events/events.gateway.ts
@@ -17,6 +17,7 @@ export class EventsGateway {
   constructor(private readonly eventsService: EventsService) {}
 
   private readonly logger = new Logger(EventsGateway.name);
+  private emittedMessages: string[] = [];
 
   @WebSocketServer()
   private server: Server;
@@ -24,6 +25,13 @@ export class EventsGateway {
   @SubscribeMessage('events')
   async handleEvent(@MessageBody() message: string) {
     this.server.emit('events', message);
+    this.emittedMessages.push(message);
     this.logger.log(message);
+  }
+
+  @SubscribeMessage('get_events')
+  async getEmittedMessages(): Promise<string[]> {
+    this.logger.log(this.emittedMessages);
+    return this.emittedMessages;
   }
 }
